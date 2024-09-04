b79e06bb01747a6e9e2334a7726d42224c3ee813
Karovii <159808804+Karovii@users.noreply.github.com>
2024-09-04 15:13:36 -0300

    Delete pratica directory

--- pratica/Estagio/ex5.py
+++
@@ -1,7 +0,0 @@
-string = input("Informe uma string: ")
-
-invertida = ''
-for caractere in range(len(string) - 1, -1, -1):
-    invertida += string[caractere]
-
-print("String invertida:", invertida)
